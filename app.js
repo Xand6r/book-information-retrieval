ca7ac58bd4a0a389206e39fe47ae0f630db24da7
xand6r <professor.alex88@gmail.com>
2019-04-21 18:53:35 +0300

    deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 // connect to local database if the laptop belongs to xander
 if(process.env.USERDOMAIN=="XANDER"){
   mongoose.connect("mongodb://localhost/retrieval",{useNewUrlParser:true})
-// }
+}
 // otherwise connect to mlabs
 else{
   mongoose.connect("mongodb://xand6r:o4kasibe@ds061196.mlab.com:61196/book_retrieval",{useNewUrlParser:true})
